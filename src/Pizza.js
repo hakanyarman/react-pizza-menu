7342dcb982bfe118f8353d8e6e1f7ab4e15d742a
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2023-07-22 15:22:08 +0300

    conditional text rendering

--- src/Pizza.js
+++ src/Pizza.js
@@ -10,7 +10,7 @@ function Pizza(props) {
             <div className='inner-container' style={{ width: 400, height: 300 }}>
                 <h1>{props.name}</h1>
                 <p>Ingredients: {props.ingredients}</p>
-                <p>Price: {props.price}$</p>
+                <p> {props.soldOut === false ? "Price :" + props.price + "$" : null}</p>
                 <img src={props.photoName} alt='pizzaimage' style={{ width: 200, height: 200 }}
                     className={props.soldOut === true ? 'fade' : 'normal'}></img>
                 <p>Sold Out: {props.soldOut.toString()}</p>
